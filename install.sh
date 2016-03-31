79406eefd264a7a83d2bf8f0ac5c4589eb4f5c32
Tianwei Shen <shentianweipku@gmail.com>
2016-03-31 12:58:52 +0800

    add cmake 3.5.1 and update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,9 @@ cp vimrc ~/.vimrc   # copy the vim configuration file
 rm -rf ~/.vim       # delete old vim settings if any 
 rsync -av vim ~/    # copy the new one
 mv ~/vim ~/.vim     # change the folder name
+
+# install the cmake 3.5.1
+cd cmake-3.5.1
+./bootstrap && make && sudo make install
+
+echo "complete!"
