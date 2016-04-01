8ab48fc726c3d83bc6ea7c04563bbf4cfbd973cd
Tianwei Shen <shentianweipku@gmail.com>
2016-04-01 19:50:03 +0800

    add bash settings for linux

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+elif [[ "$unamestr" == 'FreeBSD' ]]; then
+   platform='freebsd'
+fi
+
 # vim configuration
 cp vimrc ~/.vimrc   # copy the vim configuration file
 rm -rf ~/.vim       # delete old vim settings if any 
@@ -19,5 +27,11 @@ fi
 sudo tar -vxjf sublime_text_3_build_3103_x64.tar.bz2 -C /opt
 sudo ln -s /opt/sublime_text_3/sublime_text /usr/bin/sublime3
 sudo cp /opt/sublime_text_3/sublime_text.desktop /usr/share/applications/
+
+# set up bash
+if [[ $platform == 'linux' ]]; then
+	cp bash/bashrc_centos ~/.bashrc
+	cp bash/bash_profile_centos ~/.bash_profile
+fi
 
 echo "complete!"
