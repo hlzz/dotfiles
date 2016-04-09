010dd564686e2e17e2df0db7dfa3c68d9d079bc7
Tianwei Shen <shentianweipku@gmail.com>
2016-04-09 16:49:52 +0800

    reorganize project structure, add vtk-7.0.0

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ elif [[ "$unamestr" == 'FreeBSD' ]]; then
    platform='freebsd'
 fi
 
-########################################################
-# 		Editor settings			       #
-########################################################
+#############################################
+#			Editor settings			       	#
+#############################################
 # vim configuration
 cd editor
 cp vimrc ~/.vimrc   # copy the vim configuration file
@@ -32,26 +32,38 @@ sudo tar -vxjf sublime_text_3_build_3103_x64.tar.bz2 -C /opt
 sudo ln -s /opt/sublime_text_3/sublime_text /usr/bin/sublime3
 sudo cp /opt/sublime_text_3/sublime_text.desktop /usr/share/applications/
 cp Package\ Control.sublime-package ~/.config/sublime-text-3/Installed\ Packages/
-cd .. 	# finish editor settings
+cd .. && echo "finish editor settings"
 
-########################################################
-# 		Develop settings		       #
-########################################################
+#############################################
+# 		Develop settings					#
+#############################################
+cd dev
 # install the cmake 3.5.1
 cd cmake-3.5.1
 ./bootstrap && make && sudo make install
 
-########################################################
-# 		Workflow settings		       #
-########################################################
-# set up bash, install meshlab on linux
+# set up bash
 if [[ $platform == 'linux' ]]; then
 	cp bash/bashrc_centos ~/.bashrc
 	cp bash/bash_profile_centos ~/.bash_profile
+fi
+cd .. && echo "finish develop settings"
+
+################################################
+# 	Working environments settings		       #
+################################################
+cd graphics
+# install meshlab on linux
+if [[ $platform == 'linux' ]]; then
 	echo "Installing Meshlab"    
 	cd meshlab/src/external && qmake-qt4 -recursive external.pro && make
-    cd .. && qmake-qt4 -recursive meshlab_full.pro && make
-    # the compiled binary is in the distrib folder
+	cd .. && qmake-qt4 -recursive meshlab_full.pro && make
+    echo "The compiled meshlab binary is in the distrib folder"
 fi
+# install VTK-7.0.0
+cd VTK-7.0.0/
+mkdir build
+cmake .. && make -j4 && sudo make install
+cd .. && echo "finish graphics settings"
 
 echo "complete!"
